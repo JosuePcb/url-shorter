d3fb5ce2541ff9002bde0df29bce4412c8890823
JosuePcb <josuearvelo06@gmail.com>
2026-03-30 16:27:10 +0000

    Update url to render deploy

--- src/app/main.py
+++ src/app/main.py
@@ -12,13 +12,13 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://url-shorter-99x1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://url-shorter-99x1.onrender.com"
 
 
 class ShortenRequest(BaseModel):
--- src/public/index.js
+++ src/public/index.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://url-shorter-99x1.onrender.com";
 
 // ===== Tema =====
 const themeToggle = document.getElementById("themeToggle");
